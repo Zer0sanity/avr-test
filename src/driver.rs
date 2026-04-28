b06d8680706388446ea739466c9e5aba04328054
brittner <brittner@matthewsintl.com>
2026-04-28 07:52:34 -0500

    fixed up interupt flag stuff

--- src/driver.rs
+++ src/driver.rs
@@ -9,5 +9,5 @@ pub type TxCallback = fn() -> TxStatus;
 
 pub trait Driver {
     fn connected(&self) -> bool;
-    fn tx_submit(&self, buffer_handle: BufferHandle, length: u8);
+    fn tx_submit(&mut self, buffer_handle: BufferHandle, length: u8);
 }
--- src/usb_ft240.rs
+++ src/usb_ft240.rs
@@ -1,14 +1,19 @@
-use core::{any::Any, cell::RefCell, sync::atomic::Ordering, task::{Context, Poll}};
-use crate::{BufferHandle, SyncUnsafeCell, TxStatus, hal::Pin};
-use crate::wait_pin_state::WaitPinState;
 use crate::driver::*;
+use crate::wait_pin_state::WaitPinState;
+use crate::{BufferHandle, SyncUnsafeCell, TxStatus, hal::Pin};
+use core::{
+    any::Any,
+    cell::RefCell,
+    sync::atomic::Ordering,
+    task::{Context, Poll},
+};
 
 use avr_device::{at90can128, interrupt::Mutex};
 use avr_hal_generic::port::{self, mode};
 use portable_atomic::{AtomicPtr, AtomicU8, AtomicUsize};
 
-
-
+const TX_EXT_INT5: u8 = 1 << 5;
+const RX_EXT_INT6: u8 = 1 << 6;
 
 pub struct UsbFT240 {
     siwu: Pin<mode::Output>, //SIWU Output to tell the FT240 to flush its transmit FIFO buffer to the PC
@@ -22,7 +27,7 @@ pub struct UsbFT240 {
 }
 
 impl UsbFT240 {
-    #[rustfmt::skip]
+    // #[rustfmt::skip]
     pub fn init(
         siwu: Pin<mode::Output>,
         rd: Pin<mode::Output>,
@@ -35,97 +40,98 @@ impl UsbFT240 {
     ) -> Self {
         // Prepare initial states: active-low pins should start High (Off)
         let mut usb = Self {
-            siwu, rd, wr, txe, rxf, sense, bus_ptr,ext_int_ptr
+            siwu,
+            rd,
+            wr,
+            txe,
+            rxf,
+            sense,
+            bus_ptr,
+            ext_int_ptr,
         };
 
         usb.siwu.set_high();
         usb.rd.set_high();
         usb.wr.set_high();
-        
+
         unsafe {
             // initialize the bus as high-impedance (Input + Pull-up)
             let bus = &*usb.bus_ptr;
             bus.ddrc().write(|w| w.bits(0x00));
             bus.portc().write(|w| w.bits(0xFF));
         }
-usb
+        usb
     }
 
-    // initialize receive interrupts
-    pub fn init_rx(&self) {
+    // enable receive interrupts
+    #[inline(always)]
+    pub fn rx_int_enable(&self) {
         // initialize receive interrupts
         let ext_int = unsafe { &*self.ext_int_ptr };
         // setup RXF(PE6/INT6) to trigger on falling edges
-        ext_int.eicrb().modify(|_, w| w.isc6().falling_edge_of_intx());
-        // clear the interrupt flag
-        ext_int.eifr().modify(|_, w| w.intf().set_bit(1<<6));
+        ext_int
+            .eicrb()
+            .modify(|_, w| w.isc6().falling_edge_of_intx());
+        // clear the INT6 interrupt flag by writing it to 1
+        ext_int
+            .eifr()
+            .write(|w| unsafe { w.intf().bits(RX_EXT_INT6) });
         // enable interrupts
-        ext_int.eimsk().modify(|_, w| w.int6.set_bit());
+        ext_int
+            .eimsk()
+            .modify(|r, w| unsafe { w.int().bits(r.int().bits() | RX_EXT_INT6) });
     }
 
-    // waits for TXE to go low.  initialize transmit interrupts and kick off the first byte
-    pub fn init_tx<F>(&mut self, data: u8) {
-        // wait for the TXE pin to go low
-        while !self.txe.is_low(){}
+    // disable receive interrupts
+    #[inline(always)]
+    pub fn rx_int_disable(&self) {
+        let ext_int = unsafe { &*self.ext_int_ptr };
+        // disable interrupts
+        ext_int
+            .eimsk()
+            .modify(|r, w| unsafe { w.int().bits(r.int().bits() | !RX_EXT_INT6) });
+        // clear the interrupt flag
+        ext_int
+            .eifr()
+            .write(|w| unsafe { w.intf().bits(RX_EXT_INT6) });
+    }
+
+    // enable transmit interrupts
+    #[inline(always)]
+    pub fn tx_int_enable(&self) {
         // initialize external interrupts
         let ext_int = unsafe { &*self.ext_int_ptr };
         // setup TXE(PE5/INT5) to trigger on falling edges
-        ext_int.eicrb().modify(|_, w| w.isc5().falling_edge_of_intx());
-        // clear the INT5 interrupt flags by writing it to true
-        ext_int.eifr().modify(|_, w| w.intf5().set_bit());
+        ext_int
+            .eicrb()
+            .modify(|_, w| w.isc5().falling_edge_of_intx());
+        // clear the INT5 interrupt flags by writing it to 1
+        ext_int
+            .eifr()
+            .write(|w| unsafe { w.intf().bits(TX_EXT_INT5) });
         // enable interrupts so we get interrupted when the FT240 can accept the next byte
-        ext_int.eimsk().modify(|_, w| w.int5().set_bit());
-        // kick off the first one
-        self.tx_byte(data);
+        ext_int
+            .eimsk()
+            .modify(|r, w| unsafe { w.int().bits(r.int().bits() | TX_EXT_INT5) });
     }
 
-    // pub async fn init_tx<F>(&mut self, tx_callback: TxCallback) {
-    //     // wait for the TXE pin to go low
-    //     WaitPinState::clear(&self.txe).await;
-    //     // initialize external interrupts
-    //     let ext_int = unsafe { &*self.ext_int_ptr };
-    //     // setup TXE(PE5/INT5) to trigger on falling edges
-    //     ext_int.eicrb().modify(|_, w| w.isc5().falling_edge_of_intx());
-    //     // clear the INT5 interrupt flags by writing it to true
-    //     ext_int.eifr().modify(|_, w| w.intf5().set_bit());
-    //     // enable interrupts so we get interrupted when the FT240 can accept the next byte
-    //     ext_int.eimsk().modify(|_, w| w.int5().set_bit());
-    //     // set the callback for the isr
-    //     TX_CALLBACK.borrow(cs).set(tx_callback);
-    //     // kick off the first one
-    //     INT5();
-    // }
-    
-    pub fn disable_rx(&self)
-{
-    let ext_int = unsafe { &*self.ext_int_ptr };
-    // disable interrupts
-    ext_int.eimsk().modify(|_, w| w.int6().clear_bit());
-    // clear the interrupt flag
-    ext_int.eifr().modify(|_, w| w.intf6().clear_bit());
-}
-    
-    pub fn disable_tx(&self)
-{
-    let ext_int = unsafe { &*self.ext_int_ptr };
-    // disable interrupts
-    ext_int.eimsk().modify(|_, w| w.int5().clear_bit());
-    // clear the interrupt flag
-    ext_int.eifr().modify(|_, w| w.intf5().clear_bit());
-}
-
-    pub fn disable(&self)
-{
-    self.disable_rx();
-    self.disable_tx();
-}
-    
-    
-
+    // disable transmit interrupts
+    #[inline(always)]
+    pub fn tx_int_disable(&self) {
+        let ext_int = unsafe { &*self.ext_int_ptr };
+        // disable interrupts
+        ext_int
+            .eimsk()
+            .modify(|r, w| unsafe { w.int().bits(r.int().bits() | !TX_EXT_INT5) });
+        // clear the interrupt flag
+        ext_int
+            .eifr()
+            .write(|w| unsafe { w.intf().bits(TX_EXT_INT5) });
+    }
 
     // This sub will preform the required operation to transmit byte to the FT240.
     // NOTE:  This sub is not thread safe and should be called with interrupts disabled if interrupts are being used.
-    pub fn tx_byte(&mut self, data: u8) {
+    pub fn write_byte(&mut self, data: u8) {
         // someone is going to cringe, but real men are unsafe
         unsafe {
             // deference the data bus
@@ -150,7 +156,7 @@ usb
     // This sub will preform the required operation to receive a byte from the FT240.
     // NOTE:  This sub is not thread safe and should be called with interrupts disabled
     // if interrupts are being used.
-    pub fn rx_byte(&mut self) -> u8 {
+    pub fn read_byte(&mut self) -> u8 {
         // someone is going to cringe, but there pronouns are her/she
         unsafe {
             // deference the data bus
@@ -173,6 +179,16 @@ usb
         }
     }
 
+    // This sub will write the byte slice to the FT240 and preform a flush
+    pub fn write(&mut self, bytes: &[u8]) {
+        // write each byte
+        bytes.into_iter().for_each(|data| {
+            self.write_byte(*data);
+        });
+        // flush
+        self.flush();
+    }
+
     //Routine pulses the SIWU(Send Immediate/PC Wake-up) line to flush the FT240s Tx FIFO to the host
     pub fn flush(&mut self) {
         //Pull the SIWU pin low
@@ -182,80 +198,48 @@ usb
         //Pull the SIWU back up
         self.siwu.set_high();
     }
-
-
-
-
-
-
-    
-    // This sub will write the bytes in the slice to the FT240 and preform a flush
-    pub fn write(&mut self, bytes: &[u8]) {
-        // write each byte
-        bytes.into_iter().for_each(|data| {
-            self.tx_byte(*data);
-        });
-        // flush
-        self.flush();
-    }
-
 }
-
 
 static TX_PTR: AtomicPtr<u8> = AtomicPtr::new(core::ptr::null_mut());
 static TX_LEN: AtomicU8 = AtomicU8::new(0); // This is your "remaining bytes" counter
 static ACTIVE_TRANSFER: SyncUnsafeCell<Option<BufferHandle>> = SyncUnsafeCell::new(None);
 
-impl Driver for UsbFT240{
-    // return the state of the sense pin 
+impl Driver for UsbFT240 {
+    // return the state of the sense pin
     fn connected(&self) -> bool {
         self.sense.is_high()
     }
 
-
-    fn tx_submit(&self, buffer_handle: BufferHandle, length: u8) {
+    fn tx_submit(&mut self, buffer_handle: BufferHandle, length: u8) {
         // wait for the TXE pin to go low
-        while !self.txe.is_low(){}
+        while !self.txe.is_low() {}
 
         let data = avr_device::interrupt::free(|_cs| {
             // get the pointer to the data
             let ptr = buffer_handle.slice.as_ptr() as *mut u8;
-            let data = unsafe{*ptr};
+            let data = unsafe { *ptr };
 
-            let ptr = unsafe{ ptr.add(1)};
-            let len = length+1;
+            let ptr = unsafe { ptr.add(1) };
+            let len = length + 1;
 
-            
             // 1. Point the ISR to the data
             TX_PTR.store(ptr, Ordering::SeqCst);
             TX_LEN.store(len, Ordering::SeqCst);
 
-          
             // 2. Give ownership of the handle to the static slot
             unsafe {
                 *ACTIVE_TRANSFER.get() = Some(buffer_handle);
             }
 
-            // initialize external interrupts
-            let ext_int = unsafe { &*self.ext_int_ptr };
-            // setup TXE(PE5/INT5) to trigger on falling edges
-            ext_int.eicrb().modify(|_, w| w.isc5().falling_edge_of_intx());
-            // clear the INT5 interrupt flags by writing it to true
-            ext_int.eifr().modify(|_, w| w.intf5().set_bit());
-            // enable interrupts so we get interrupted when the FT240 can accept the next byte
-            ext_int.eimsk().modify(|_, w| w.int5().set_bit());
+            self.tx_int_enable();
             // return the first byte
             data
         });
 
         // kick off the first one
-        self.tx_byte(data);
-
-        
+        self.write_byte(data);
     }
 }
-
-
 
 //USB tx interrupt.  I if there is data to send it will transmit the next byte to the FT240 chip otherwise it will pulse the SIWU
 //to flush the FT240s tx FIFO to the host and disable the tx interrupts.
@@ -270,23 +254,21 @@ fn INT5() {
         // Transmission finished!
         // disable_tx_interrupt();
 
-        // TAKE the handle out of the static. 
+        // TAKE the handle out of the static.
         // When 'released_handle' goes out of scope here, it DROPS.
         let _released_handle = unsafe { (*ACTIVE_TRANSFER.get()).take() };
     }
 }
-
 
 // //USB Rx Interrupt
 // #pragma vector = INT6_vect
 // __interrupt static void int6(void)
 // {
 //     //Read the data from the FT240 chip
-//     USBRxStateObject->RxData = ReceiveByteFromFT240(); 
+//     USBRxStateObject->RxData = ReceiveByteFromFT240();
 //     //Execute callback to receive the byte
-//     RxAction(); 
+//     RxAction();
 // }
-
 
 // Local Variables:
 // jinx-local-words: #("isr nop tx usb" 4 7 (jinx--group "Suggestions from session" jinx--prefix #("07 " 0 3 (face jinx-key)) jinx--suffix nil))
